702e6cdcfe57159ea1232f83c97826f3f31f4f6a
Eric Seidel <eric@shorebird.dev>
2023-08-31 21:34:45 -0700

    Clean up main a little

--- bin/space_gen.dart
+++ bin/space_gen.dart
@@ -6,12 +6,14 @@ import 'package:space_gen/space_gen.dart';
 final logger = Logger();
 
 void main(List<String> arguments) async {
-  final specUrl = Uri.parse('../api-docs/reference/SpaceTraders.json');
-  final spec = await Spec.load(specUrl);
-  // for (final endpoint in spec.endpoints) {
-  //   logger.info('${endpoint.tag} ${endpoint.method} ${endpoint.path}');
-  // }
+  // These could eventually be passed in as arguments
+  const specPath = '../api-docs/reference/SpaceTraders.json';
+  const outDirPath = 'out';
 
-  final outDir = Directory('out')..deleteSync(recursive: true);
+  // We should eventualy support urls not just paths.
+  final specUrl = Uri.parse(specPath);
+  final spec = await Spec.load(specUrl);
+  // Could make clearing of the directory optional.
+  final outDir = Directory(outDirPath)..deleteSync(recursive: true);
   Context(spec, outDir).render();
 }
