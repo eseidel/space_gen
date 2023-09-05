d9039c27cf8885b648271ef55c13dc5887cf3a47
Eric Seidel <eric@shorebird.dev>
2023-09-04 20:39:54 -0700

    Take config as argument

--- bin/space_gen.dart
+++ bin/space_gen.dart
@@ -1,24 +1,43 @@
 import 'dart:io';
 
-import 'package:path/path.dart' as p;
+import 'package:args/args.dart';
 import 'package:space_gen/space_gen.dart';
+import 'package:space_gen/src/config.dart';
 import 'package:space_gen/src/logger.dart';
 
 void main(List<String> arguments) async {
-  // These could eventually be passed in as arguments
-  const specPath = 'examples/specs/spacetraders/reference/SpaceTraders.json';
-  const outDirPath = 'examples/spacetraders';
+  // Mostly trying to match openapi-generator-cli
+  final parser = ArgParser()
+    ..addOption('config', abbr: 'c', help: 'Path to config file');
+  final results = parser.parse(arguments);
+  if (results.rest.isNotEmpty) {
+    logger.err('Unexpected arguments: ${results.rest}');
+    exit(1);
+  }
 
+  final configPath = results['config'] as String?;
+  final Config config;
+  if (configPath != null) {
+    config = loadFromFile(configPath);
+  } else {
+    logger.err('No config file provided');
+    exit(1);
+  }
+
+  final specPath = config.specUri;
+  final outDirPath = config.outDirPath;
   logger.info('Generating $specPath to $outDirPath');
-  final absolutePath = p.absolute(specPath);
-  final specUrl = Uri.parse(absolutePath);
+
   // Could make clearing of the directory optional.
   final outDir = Directory(outDirPath);
   if (outDir.existsSync()) {
     outDir.deleteSync(recursive: true);
   }
-  final context =
-      Context(specUrl: specUrl, outDir: outDir, packageName: 'spacetraders');
+  final context = Context(
+    specUrl: config.specUri,
+    outDir: outDir,
+    packageName: config.packageName,
+  );
   await context.load();
   context.render();
 }
