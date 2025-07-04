fdd2e89e20cfb2c3a622c46e15a189e03c4f07e1
Eric Seidel <eric@shorebird.dev>
2025-07-03 21:28:21 -0700

    test: non-nullable uri-template

--- test/render/schema_renderer_test.dart
+++ test/render/schema_renderer_test.dart
@@ -302,6 +302,63 @@ void main() {
           '}\n',
         );
       });
+
+      test('non-nullable', () {
+        final schema = {
+          'type': 'object',
+          'properties': {
+            'foo': {'type': 'string', 'format': 'uri-template'},
+          },
+          'required': ['foo'],
+        };
+        final result = renderTestSchema(schema);
+        expect(
+          result,
+          '@immutable\n'
+          'class Test {\n'
+          '    Test(\n'
+          '        { required this.foo, \n'
+          '         }\n'
+          '    );\n'
+          '\n'
+          '    factory Test.fromJson(Map<String, dynamic>\n'
+          '        json) {\n'
+          '        return Test(\n'
+          "            foo: UriTemplate(json['foo'] as String),\n"
+          '        );\n'
+          '    }\n'
+          '\n'
+          '    /// Convenience to create a nullable type from a nullable json object.\n'
+          '    /// Useful when parsing optional fields.\n'
+          '    static Test? maybeFromJson(Map<String, dynamic>? json) {\n'
+          '        if (json == null) {\n'
+          '            return null;\n'
+          '        }\n'
+          '        return Test.fromJson(json);\n'
+          '    }\n'
+          '\n'
+          '    final UriTemplate foo;\n'
+          '\n'
+          '    Map<String, dynamic> toJson() {\n'
+          '        return {\n'
+          "            'foo': foo.toString(),\n"
+          '        };\n'
+          '    }\n'
+          '\n'
+          '    @override\n'
+          '    int get hashCode =>\n'
+          '          foo.hashCode;\n'
+          '\n'
+          '    @override\n'
+          '    bool operator ==(Object other) {\n'
+          '        if (identical(this, other)) return true;\n'
+          '        return other is Test\n'
+          '            && this.foo == other.foo\n'
+          '        ;\n'
+          '    }\n'
+          '}\n',
+        );
+      });
     });
 
     test('allOf', () {
