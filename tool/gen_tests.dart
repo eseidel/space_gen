11cafde548000f2dff634997d111a9e527366454
Eric Seidel <eric@shorebird.dev>
2025-10-18 14:17:48 -0700

    chore: disable private tests for now

--- tool/gen_tests.dart
+++ tool/gen_tests.dart
@@ -3,7 +3,12 @@ import 'package:file/local.dart';
 import 'package:path/path.dart' as p;
 import 'package:space_gen/space_gen.dart';
 
-Future<void> genTests(Directory testDir) async {
+Future<void> genTests(
+  Directory testDir, {
+  List<String> skipList = const [],
+}) async {
+  const quirks = Quirks.openapi();
+  final templatesDir = const LocalFileSystem().directory('lib/templates');
   // Collect all the spec.json files in the directory.
   final specFiles = testDir
       .listSync()
@@ -12,13 +17,19 @@ Future<void> genTests(Directory testDir) async {
   for (final specFile in specFiles) {
     // Grab the name of the spec from the file name.
     final specName = p.basenameWithoutExtension(specFile.path);
-    final outDir = testDir.childDirectory(specName);
+    if (skipList.contains(specName)) {
+      logger.info('Skipping $specName');
+      continue;
+    }
+    logger.info('Generating from $specFile to ${testDir.path}');
+    final packageName = specName.replaceAll('.', '_').replaceAll('-', '_');
+    final outDir = testDir.childDirectory(packageName);
     await loadAndRenderSpec(
       specUrl: specFile.uri,
       outDir: outDir,
-      packageName: specName,
-      quirks: const Quirks.openapi(),
-      templatesDir: const LocalFileSystem().directory('lib/templates'),
+      packageName: packageName,
+      quirks: quirks,
+      templatesDir: templatesDir,
     );
   }
 }
@@ -27,21 +38,22 @@ Future<void> run() async {
   const fs = LocalFileSystem();
   final packageRoot = fs.currentDirectory;
   final publicDir = packageRoot.childDirectory('../gen_tests');
-  final privateDir = packageRoot.childDirectory('../private_gen_tests');
+  // final privateDir = packageRoot.childDirectory('../private_gen_tests');
+  final skipList = ['petstore'];
 
   if (publicDir.existsSync()) {
-    await genTests(publicDir);
+    await genTests(publicDir, skipList: skipList);
   } else {
     logger.err('gen_tests directory does not exist: ${publicDir.path}');
     return;
   }
-  if (privateDir.existsSync()) {
-    await genTests(privateDir);
-  } else {
-    logger.info(
-      'private_gen_tests directory does not exist: ${privateDir.path}',
-    );
-  }
+  // if (privateDir.existsSync()) {
+  //   await genTests(privateDir);
+  // } else {
+  //   logger.info(
+  //     'private_gen_tests directory does not exist: ${privateDir.path}',
+  //   );
+  // }
 }
 
 void main() async {
